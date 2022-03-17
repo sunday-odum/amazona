52de5c919175b6d0d069d50c20c302ab1153f9db
sunday odum <sundayodum@gmail.com>
2022-03-17 13:01:53 +0100

    create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      <a href="/">Amazona</a>
+      </header>
+      <main> product list</main>
+    </div>
+  );
+}  
+
+export default App;
