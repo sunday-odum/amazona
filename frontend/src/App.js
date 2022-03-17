0949b002b06452e0a39f1b5f200fcfdd57c85bfa
sunday odum <sundayodum@gmail.com>
2022-03-17 22:01:30 +0100

    list product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,38 @@
-
+import data from "./data";
+import './index.css'
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <header>
       <a href="/">Amazona</a>
       </header>
-      <main> product list</main>
+      <main>
+          <h1>Featured Products</h1>
+      <div className="products">
+       {
+        data.products.map(product => (
+        
+        <div className="product" key={product.slug}>
+          <a href={`/product/${product.slug}`}>
+          <img src={product.image} alt={product.name} />
+          </a>
+          <div className="product-info">
+            <a href={`/product/${product.slug}`}>
+            <p>{product.name}</p>
+            </a>
+          <p>
+            <strong>${product.price}</strong>
+          </p>
+          <button>Add to cart</button>
+          </div>
+          </div>))
+        }
+        </div>
+    
+      </main>
     </div>
   );
-}  
+};  
 
 export default App;
